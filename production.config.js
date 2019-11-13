e93d50ee52975a5a35cde3ecb8dad073c6925e60
Chugou9 <yesman3141@gmail.com>
2019-11-13 10:24:58 +0300

    bundle: Добавлены плагины, разделены конфиги
    - добавлены htmWebpackPlugin, cleanWebpackPlugin
    - добавлены 2 точки входа
    - разделены конфиги

---
+++ production.config.js
@@ -0,0 +1,7 @@
+const path = require("path");
+
+module.exports = {
+    mode: "production",
+    fileName: "main.js",
+    path: path.resolve(__dirname, "dist")
+}
---
+++ webpack.config.js
@@ -0,0 +1,21 @@
+const path = require('path');
+const HtmlWebpackPlugin = require("html-webpack-plugin");
+const {CleanWebpackPlugin} = require('clean-webpack-plugin');
+
+module.exports = {
+    mode: "development",
+    entry: {
+        application: "./src/index.js",
+        introduction: "./src/intro.js"
+    },
+    output: {
+        filename: '[name].bundle.js',
+        path: path.resolve(__dirname, 'dist'),
+    },
+    plugins: [
+        new HtmlWebpackPlugin({
+            title: 'Output Management',
+        }),
+        new CleanWebpackPlugin()
+    ]
+};
